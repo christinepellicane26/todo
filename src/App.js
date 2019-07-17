2ac51e286b5835bb9ecbc060a61a5119def5a7d9
christinepellicane26 <christinepellicane@gmail.com>
2019-07-17 17:04:27 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+export default App;import React, {Component} from 'react';
+import * as Components from './components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class ToDoList extends Component {
+constructor(){
+  super();
+  this.state = {
+    todos: [],
+    currentToDo: "",
+    }
+  }
+
+  handleChange = event => {
+    this.setState({currentToDo: event.target.value});
+  }
+
+  addItem = event => {
+    event.preventDefault();
+    console.log("addItem Method Fired");
+    this.setState({
+      currentToDo: "",
+      todos:[...this.state.todos,this.state.currentToDo]})
+  }
+  
+  addCurrentToDo = event => {
+    
+  }
+
+
+  deleteCurrentToDo = index => {
+    console.log(index) 
+    // this.state.todo.filter(el => el !== name)
+
+  
 }
+
+
+  render() {
+    console.log(this.state)
+    const theList = this.state.todos.map((todos, index) => <li>{todos}
+    <button type ="delete" onClick= {()=>this.deleteCurrentToDo(index)}> Delete completed task</button>
+    </li>);
+    
+    return (
+    
+    
+
+    // </div> 
+    // <Link to = "/examplelink"> Go to examplelink!</Link>
+    // let id = this.props.match.params.name -> will match url bar
+    //   <div>
+      <div>
+        <form onSubmit={this.addItem}>
+          <label htmlFor="Add To Box">Add To-Do Item:</label>
+          <input
+            type="text"
+            name="currentTodo"
+            value={this.state.currentToDo}
+            onChange={this.handleChange}
+          />
+          <button type ="submit" onClick= {this.addCurrentToDo}> Add new task</button>
+                       
+        </form>
+
+        <div class = 'Show list'>
+          
+          <ul>
+           <h1>{theList}</h1>
+           
+           
+          </ul>
+
+        </div>
+    </div>
+    );
+}
+
+}
+
+
+
+    
 
 export default App;
